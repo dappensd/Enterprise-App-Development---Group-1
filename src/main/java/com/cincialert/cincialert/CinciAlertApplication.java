6d99d1ad8cf3a23ed12f345ecc34d1df677cbe2b
Carson Simms <143337976+Carson-Simms@users.noreply.github.com>
2023-10-05 17:32:08 -0400

    Initial Commit, Added starter springboot files

---
+++ src/main/java/com/cincialert/cincialert/CinciAlertApplication.java
@@ -0,0 +1,13 @@
+package com.cincialert.cincialert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinciAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinciAlertApplication.class, args);
+	}
+
+}
