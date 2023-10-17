6483e53c33f212692516079c615571e3dde33d96
ZachtMac <zachary.t.mcintosh@outlook.com>
2023-10-17 13:07:54 -0400

    Added Javadoc

--- src/main/java/com/cincialert/cincialert/CinciAlertApplication.java
+++ src/main/java/com/cincialert/cincialert/CinciAlertApplication.java
@@ -3,6 +3,17 @@ package com.cincialert.cincialert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The CinciAlertApplication class is the main entry point for the CinciAlert application.
+ * It initializes and starts the Spring Boot application.
+ *
+ * This class is annotated with {@code @SpringBootApplication}, which indicates that it's a Spring Boot
+ * application and includes all the necessary configuration and setup.
+
+ * @author [Insert Name]
+ * @version 1.0
+ * @since [Date]
+ */
 @SpringBootApplication
 public class CinciAlertApplication {
 
--- src/main/java/com/cincialert/cincialert/CinciAlertController.java
+++ src/main/java/com/cincialert/cincialert/CinciAlertController.java
@@ -4,6 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ * The CinciAlertController class is responsible for handling HTTP requests and
+ * rendering the corresponding web pages for the CinciAlert application.
+ *
+ * This controller is annotated with {@code @Controller}, which indicates that it
+ * plays a role in processing web requests. It defines three request mappings for the
+ * root ("/"), "/login", and "/signup" paths, each of which handles specific web page
+ * requests.
+ *
+ * @author [Insert Name]
+ * @version 1.0
+ * @since [Date]
+ */
 @Controller
 public class CinciAlertController {
     @RequestMapping("/")
