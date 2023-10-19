cb428e71422f3b0632e54902dd30eb14eaf307e4
colton <daltonco@mail.uc.edu>
2023-10-18 22:20:41 -0400

    Update CinciAlertApplicationTests.java
    added most of a test to verify trafficIncident properties.
    
    TODO Add test condition for the Location property. All other properties tested for.

--- src/test/java/com/cincialert/cincialert/CinciAlertApplicationTests.java
+++ src/test/java/com/cincialert/cincialert/CinciAlertApplicationTests.java
@@ -3,6 +3,12 @@ package com.cincialert.cincialert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.xml.stream.Location;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CinciAlertApplicationTests {
 
@@ -10,4 +16,28 @@ class CinciAlertApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void verifyTrafficIncidentProperties(){
+		int IncidentID = 1;
+		List<String> Location = new ArrayList<String>();
+		String Description = "1";
+		String Severity = "1";
+
+		//Create new TrafficIncident
+		TrafficIncident trafficIncident = new TrafficIncident();
+
+		//set & assert IncidentID
+		trafficIncident.setIncidentID(IncidentID);
+		assertEquals(IncidentID, trafficIncident.getIncidentID());
+
+		// TODO Add Location test condition
+
+		//set & assert Description
+		trafficIncident.setDescription(Description);
+		assertEquals(Description, trafficIncident.getDescription());
+
+		//set & assert Severity
+		trafficIncident.setSeverity(Severity);
+		assertEquals(Severity, trafficIncident.getSeverity());
+	}
 }
