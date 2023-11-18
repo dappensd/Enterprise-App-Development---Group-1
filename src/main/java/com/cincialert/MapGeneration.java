f8ea55ac49852618a92b48fd0759008cf8972990
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2023-11-18 15:56:50 -0500

    Moved map generation to a java class, added google dependencies

---
+++ src/main/java/com/cincialert/MapGeneration.java
@@ -0,0 +1,10 @@
+package com.cincialert;
+import com.google.maps.GeoApiContext;
+import com.google.maps.DirectionsApi;
+import com.google.maps.model.DirectionsResult;
+
+public class MapGeneration {
+    public static void main(String[] args) {
+
+    }
+}
