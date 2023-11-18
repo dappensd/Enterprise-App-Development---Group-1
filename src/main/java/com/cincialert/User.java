5bc1cc0628b149059fc1b61dd25e9b67109606f1
Samuel Dappen <dappensd@gmail.com>
2023-11-17 19:46:17 -0500

    Updated user constructor

---
+++ src/main/java/com/cincialert/User.java
@@ -0,0 +1,98 @@
+package com.cincialert;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+
+    private boolean emailNotifications;
+    private boolean pushNotifications;
+    private boolean smsNotifications;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.emailNotifications = true; // Default to true for simplicity
+        this.pushNotifications = true;
+        this.smsNotifications = true;
+    }
+
+    // Getter and setter methods
+
+    public boolean register() {
+        // Implement user registration logic
+        // For simplicity, let's assume successful registration every time
+        return true;
+    }
+
+    public boolean login(String enteredUsername, String enteredPassword) {
+        // Implement user login logic
+        return username.equals(enteredUsername) && password.equals(enteredPassword);
+    }
+
+    public void updateProfile(boolean emailNotifications, boolean pushNotifications, boolean smsNotifications) {
+        // Implement profile update logic
+        this.emailNotifications = emailNotifications;
+        this.pushNotifications = pushNotifications;
+        this.smsNotifications = smsNotifications;
+    }
+
+    // Notification-related methods
+    public void enableEmailNotifications() {
+        emailNotifications = true;
+    }
+
+    public void disableEmailNotifications() {
+        emailNotifications = false;
+    }
+
+    public void enablePushNotifications() {
+        pushNotifications = true;
+    }
+
+    public void disablePushNotifications() {
+        pushNotifications = false;
+    }
+
+    public void enableSmsNotifications() {
+        smsNotifications = true;
+    }
+
+    public void disableSmsNotifications() {
+        smsNotifications = false;
+    }
+
+    public void sendNotification(String message) {
+        NotificationManager notificationManager = new NotificationManager();
+        notificationManager.sendNotification(this, message);
+    }
+
+    // Getter methods for notification preferences
+    public boolean isEmailNotifications() {
+        return emailNotifications;
+    }
+
+    public boolean isPushNotifications() {
+        return pushNotifications;
+    }
+
+    public boolean isSmsNotifications() {
+        return smsNotifications;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
+
