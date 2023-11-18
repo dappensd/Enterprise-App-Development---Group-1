910b27c458b75e9469860058294bd0e0dd0ad8f4
Samuel Dappen <dappensd@gmail.com>
2023-11-17 19:46:01 -0500

    Created Notifcation Manager class and associated methods

---
+++ src/main/java/com/cincialert/NotificationManager.java
@@ -0,0 +1,21 @@
+package com.cincialert;
+
+public class NotificationManager {
+    public void sendNotification(User user, String message) {
+        // Implement notification sending logic based on user preferences
+        if (user.isEmailNotifications()) {
+            // Implement email notification logic
+            System.out.println("Sending email notification to " + user.getEmail() + ": " + message);
+        }
+        if (user.isPushNotifications()) {
+            // Implement push notification logic
+            System.out.println("Sending push notification to " + user.getUsername() + ": " + message);
+        }
+        if (user.isSmsNotifications()) {
+            // Implement SMS notification logic
+            System.out.println("Sending SMS notification to " + user.getUsername() + ": " + message);
+        }
+    }
+
+}
+
