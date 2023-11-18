dd01681f9df9d6d630d4e2d511f74ddb6b4c652e
Samuel Dappen <dappensd@gmail.com>
2023-11-17 19:46:41 -0500

    Created TrafficAlertManager class and associated methods

---
+++ src/main/java/com/cincialert/TrafficAlertManager.java
@@ -0,0 +1,37 @@
+package com.cincialert;
+
+import java.util.List;
+import com.cincialert.dto.TrafficIncident;
+
+public class TrafficAlertManager {
+    private List<TrafficIncident> incidentList;
+
+    public TrafficAlertManager(List<TrafficIncident> incidentList) {
+        this.incidentList = incidentList;
+    }
+
+    public void reportIncident(TrafficIncident incident) {
+        incidentList.add(incident);
+        notifyUsers(incident);
+    }
+
+    public List<TrafficIncident> getIncidentList() {
+        return incidentList;
+    }
+
+    private void notifyUsers(TrafficIncident incident) {
+        NotificationManager notificationManager = new NotificationManager();
+
+        for (User user : getUsersToNotify()) {
+            notificationManager.sendNotification(user, "Traffic incident reported: " + incident.getDescription());
+        }
+    }
+
+    private List<User> getUsersToNotify() {
+        return List.of(new User("user1", "user1@example.com", "password1"),
+                new User("user2", "user2@example.com", "password2"));
+    }
+}
+
+
+
