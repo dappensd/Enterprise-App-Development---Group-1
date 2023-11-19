9c85b95a83bf75a7f82b826b543bfeb5336219e3
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2023-11-19 15:39:29 -0500

    Map should talk to DTO via a repository

---
+++ src/main/java/com/cincialert/MapController.java
@@ -0,0 +1,26 @@
+package com.cincialert.controller;
+
+import com.cincialert.dto.TrafficIncident;
+import com.cincialert.repository.TrafficIncidentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class MapController {
+
+    @Autowired
+    private TrafficIncidentRepository trafficIncidentRepository;
+
+    @GetMapping("/map")
+    public String showMap(Model model) {
+        List<TrafficIncident> trafficIncidents = trafficIncidentRepository.findAll();
+        model.addAttribute("trafficIncidents", trafficIncidents);
+        return "map";
+    }
+}
+
+
