3a60822136bec570cababe753eed7f1b5244bd98
David <torandc@mail.uc.edu>
2023-12-03 05:17:41 -0500

    add validation check to create report endpoint

--- src/main/java/com/cincialert/controllers/TrafficIncidentController.java
+++ src/main/java/com/cincialert/controllers/TrafficIncidentController.java
@@ -2,6 +2,12 @@ package com.cincialert.controllers;
 
 import com.cincialert.dto.TrafficIncident;
 import com.cincialert.service.ITrafficIncidentService;
+import com.cincialert.utils.MapInfo;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +17,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @Controller
 public class TrafficIncidentController {
+
     @Autowired
     ITrafficIncidentService incidentService;
+
     @RequestMapping("/submitIncident")
-    public String sendReportPage(Model model) {
+    public String sendReportPage(Model model)  {
 
         model.addAttribute(new TrafficIncident());
         return "traffic-report-send";
@@ -25,12 +34,29 @@ public class TrafficIncidentController {
     @RequestMapping("/createReport")
     public ResponseEntity createReport(TrafficIncident report) {
         try {
+            GeoApiContext context = new GeoApiContext.Builder()
+                    .apiKey("AIzaSyCoVLMMQ_OgyrsklHvh9L6-x8hvyC_fspk")
+                    .build();
 
-            incidentService.save(report);
 
-            return new ResponseEntity(HttpStatus.OK);
-        } catch (Exception exception) {
-            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            LatLng reportCoords = new LatLng(report.getLatitude(), report.getLongitude());
+            GeocodingResult[] results = GeocodingApi.reverseGeocode(context, reportCoords).await();
+
+            boolean isReportWithinCincy = MapInfo.isCoordinateWithinCincy(reportCoords);
+            boolean isTooFarFromStreet = MapInfo.isCoordinateTooFarFromStreet(results, new LatLng(report.getLatitude(), report.getLongitude()));
+            boolean isReportNearIncident = MapInfo.isReportIncidentNearOtherIncident(reportCoords, incidentService.fetchAll());
+
+            if (isReportWithinCincy && !isTooFarFromStreet && !isReportNearIncident){
+                incidentService.save(report);
+
+            }else{
+                // TODO: Add logic to send sms or email to user that the traffic submission not valid
+            }
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
