6e4e7b3cfca540d3fcd0ffa34baa321d21849015
David <torandc@mail.uc.edu>
2023-12-03 04:58:20 -0500

    added distance calculation

--- src/main/java/com/cincialert/utils/MapInfo.java
+++ src/main/java/com/cincialert/utils/MapInfo.java
@@ -16,5 +16,21 @@ public class MapInfo {
                 && position.lng <= maxLongitude);
     }
 
+    public static double getDistanceBetweenPoints(LatLng point1, LatLng point2) {
+        final double radius = 6371;
+
+        double lat1Rad = Math.toRadians(point1.lat);
+        double lat2Rad = Math.toRadians(point2.lat);
+        double lon1Rad = Math.toRadians(point1.lng);
+        double lon2Rad = Math.toRadians(point2.lng);
+
+        double x = (lon2Rad - lon1Rad) * Math.cos((lat1Rad + lat2Rad) / 2);
+        double y = (lat2Rad - lat1Rad);
+
+        double distance = Math.sqrt(x * x + y * y) * radius * 1000;
+
+        return distance;
+    }
+
 
 }
